f3dc28044cd0cc49994afa9b6edac6ac2dd7d199
mhpranta12 <mhpranta2@gmail.com>
2025-05-28 19:53:38 +0600

    Initial Project Add

---
+++ Chatbot/Chatbot.Domain/Core/Entities/Messages/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatbot.Domain.Core.Entities.Messages
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+        public string? ContentURL { get; set; }
+        public Guid UserId { get; set; }
+        public Message(string text, string contentUrl)
+        {
+            Text = text;
+            ContentURL = contentUrl;
+        }
+    }
+}
